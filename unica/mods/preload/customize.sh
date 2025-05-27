830d75165342148ddf4c904546c3bacd3f40b536
Muhammad Gudaji <gudajimuhammaf@gmail.com>
2025-05-27 10:52:59 +0100

    Init for s9

--- unica/mods/preload/customize.sh
+++ unica/mods/preload/customize.sh
@@ -10,6 +10,10 @@ DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.popupcalcula
 DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.clockpackage")" \
     "$WORK_DIR/system/system/preload/ClockPackage/ClockPackage.apk"
 
+# Samsung Internet
+DOWNLOAD_APK "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.sbrowser")" \
+     "SBrowser/SBrowser.apk"
+
 # Samsung Voice Recorder
 DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.sec.android.app.voicenote")" \
     "$WORK_DIR/system/system/preload/VoiceNote_5.0/VoiceNote_5.0.apk"
